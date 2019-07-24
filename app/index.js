0fba128c65fc21744e6ceac37074a1081fe600e5
shomail <shomail1988@gmail.com>
2019-07-24 16:39:45 +0200

    initial commit

---
+++ app/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import './index.css';
+// import Popluar from './components/Popular';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='container'>
+        <h1>Github Battleground</h1>
+        {/* <Popluar /> */}
+      </div>
+    )
+  }
+}
+
+ReactDom.render(<App />, document.getElementById('app'));
