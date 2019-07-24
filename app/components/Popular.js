c524e6c66c4bc91ff1445f55106bbfb3199ae6cb
shomail <shomail1988@gmail.com>
2019-07-24 16:58:47 +0200

    change Popular tags done

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,13 +1,34 @@
 import React from 'react';
 
 export default class Popular extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedLanguage: 'All',
+        }
+
+        this.updateLanguage = this.updateLanguage.bind(this)
+    }
+    updateLanguage(selectedLanguage) {
+        this.setState({
+            selectedLanguage
+        })
+    }
+
     render() {
         const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
         return (
             <ul className='flex-center'>
                 {languages.map((lang) => (
                     <li key={lang}>
-                        <button type="button" className='btn-clear nav-link'>{lang}</button>
+                        <button 
+                        type="button" 
+                        className='btn-clear nav-link'
+                        style={lang === this.state.selectedLanguage ? {color: 'lightcoral'}: null} 
+                        onClick={() => this.updateLanguage(lang)}
+                        >
+                            {lang}
+                        </button>
                     </li>
                 ))}
             </ul>
