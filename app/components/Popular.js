56b680f36cddc09170553cdd913c442931bb3e38
shomail <shomail1988@gmail.com>
2019-07-25 09:51:54 +0200

    fetch repos on nav change

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { fetchPopularRepos } from '../utils/api';
 
 function LanguagesNav({selected, onUpdateLanguage}) {
     const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -31,21 +32,50 @@ export default class Popular extends React.Component {
         super(props);
         this.state = {
             selectedLanguage: 'All',
+            repos: null,
+            error: null,
         }
 
         this.updateLanguage = this.updateLanguage.bind(this)
+        this.isLoading = this.isLoading.bind(this)
     }
     updateLanguage(selectedLanguage) {
         this.setState({
-            selectedLanguage
+            selectedLanguage,
+            erroor: null,
+            repos: null,
         })
+
+        fetchPopularRepos(selectedLanguage)
+            .then((repos) => this.setState({
+                repos,
+                error: null,
+            }))
+            .catch((error) => {
+                console.warn('Error fetching repos: ', error)
+                this.setState({
+                    error: `There was an error fetching the repositories.`,
+                })
+            })
+    }
+
+    isLoading() {
+        return this.state.repos === null && this.state.error === null;
     }
 
     render() {
-        const {selectedLanguage} = this.state;
+        const { selectedLanguage, repos, error } = this.state;
         return (
             <>
-                <LanguagesNav selected={selectedLanguage} onUpdateLanguage={this.updateLanguage} />
+                <LanguagesNav 
+                    selected={selectedLanguage} 
+                    onUpdateLanguage={this.updateLanguage} 
+                />
+                {this.isLoading() && <p>LOADING...</p>}
+
+                {error && <p>{error}</p>}
+
+                {repos && <pre>{JSON.stringify(repos, null, 2)}</pre>}
             </>
         )
     }
