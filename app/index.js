6f7c0bfdd7c327f9a3653f893f6573a306936ddd
shomail <shomail1988@gmail.com>
2019-07-26 09:17:53 +0200

    Battle componenet instructions

--- app/index.js
+++ app/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css';
 import Popluar from './components/Popular';
+import Battle from './components/Battle';
 
 class App extends React.Component {
   render() {
     return (
       <div className='container'>
         <h1>Github Explorer</h1>
-        <Popluar />
+        <Battle />
       </div>
     )
   }
