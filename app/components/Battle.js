f4594a27575292fd819dbd6a6a9a8174b583ee56
shomail <shomail1988@gmail.com>
2019-07-26 09:40:28 +0200

    player input component

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {FaUserFriends, FaFighterJet, FaTrophy} from 'react-icons/fa';
+import PropTypes from 'prop-types';
 
 const Instructions = () => (
     <div className="instructions-container">
@@ -22,6 +23,60 @@ const Instructions = () => (
         </ol>
     </div>
 );
+
+class PlayerInput extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            username: ''
+        }
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleSubmit(e) {
+        e.preventDefault();
+        this.props.onSubmit(this.state.username);
+    }
+    handleChange(e) {
+        this.setState({
+            username: e.target.value
+        })
+    }
+    render() {
+        return (
+            <form className="column player" onSubmit={this.handleSubmit}>
+                <label htmlFor="username" className="player-label">
+                    {this.props.label}
+                </label>
+                <div className="row player-inputs">
+                    <input 
+                        type="text" 
+                        id="username" 
+                        className="input-light" 
+                        placeholder="Github Username" 
+                        autoComplete="off"  
+                        value={this.state.username}
+                        onChange={this.handleChange}
+                    />
+                    <button 
+                        type="submit"
+                        className="btn btn-dark"
+                        disabled={!this.state.username}
+                    >
+                        Submit
+                    </button>
+                </div>
+            </form>
+        )
+    }
+}
+
+PlayerInput.propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+    label: PropTypes.string.isRequired,
+}
 
 export default class Battle extends React.Component {
     render() {
