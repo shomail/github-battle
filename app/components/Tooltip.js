bfe6d9b5721d30bb867988447df4b78a8775d849
shomail <shomail1988@gmail.com>
2019-08-01 09:22:47 +0200

    refactor Tooltip to use HOC

--- app/components/Tooltip.js
+++ app/components/Tooltip.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import withHover from './withHover';
 
 const styles = {
   container: {
@@ -22,51 +23,64 @@ const styles = {
     fontSize: '14px',
   },
 };
+// CLASS BASED TOOLTIP COMPONENT, hover logic is coupled in this component and we cant use it with some other component
+// export default class Tooltip extends React.Component {
+//   constructor(props) {
+//     super(props);
 
-export default class Tooltip extends React.Component {
-  constructor(props) {
-    super(props);
+//     this.state = {
+//       hovering: false,
+//     };
 
-    this.state = {
-      hovering: false,
-    };
+//     this.mouseOver = this.mouseOver.bind(this);
+//     this.mouseOut = this.mouseOut.bind(this);
+//   }
 
-    this.mouseOver = this.mouseOver.bind(this);
-    this.mouseOut = this.mouseOut.bind(this);
-  }
+//   mouseOver() {
+//     this.setState({
+//       hovering: true,
+//     });
+//   }
 
-  mouseOver() {
-    this.setState({
-      hovering: true,
-    });
-  }
+//   mouseOut() {
+//     this.setState({
+//       hovering: false,
+//     });
+//   }
 
-  mouseOut() {
-    this.setState({
-      hovering: false,
-    });
-  }
+//   render() {
+//     const { text, children } = this.props;
+//     const { hovering } = this.state;
 
-  render() {
-    const { text, children } = this.props;
-    const { hovering } = this.state;
+//     return (
+//       <div
+//         onFocus={this.mouseOver}
+//         onBlur={this.mouseOut}
+//         onMouseOver={this.mouseOver}
+//         onMouseOut={this.mouseOut}
+//         style={styles.container}
+//       >
+//         {hovering === true && <div style={styles.tooltip}>{text}</div>}
+//         {children}
+//       </div>
+//     );
+//   }
+// }
 
-    return (
-      <div
-        onFocus={this.mouseOver}
-        onBlur={this.mouseOut}
-        onMouseOver={this.mouseOver}
-        onMouseOut={this.mouseOut}
-        style={styles.container}
-      >
-        {hovering === true && <div style={styles.tooltip}>{text}</div>}
-        {children}
-      </div>
-    );
-  }
+// to reuse hover logic we can use HOC
+function Tooltip({ text, children, hovering }) {
+  return (
+    <div style={styles.container}>
+      {hovering === true && <div style={styles.tooltip}>{text}</div>}
+      {children}
+    </div>
+  );
 }
 
 Tooltip.propTypes = {
   text: PropTypes.string.isRequired,
-  children: PropTypes.element,
+  children: PropTypes.element.isRequired,
+  hovering: PropTypes.bool.isRequired,
 };
+
+export default withHover(Tooltip);
