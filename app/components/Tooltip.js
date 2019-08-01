f3a5286ae5e8cce15db5d38419fd17c2a256e2b3
shomail <shomail1988@gmail.com>
2019-08-01 10:00:26 +0200

    refactor Tooltip to use render prop component

--- app/components/Tooltip.js
+++ app/components/Tooltip.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import withHover from './withHover';
+import Hover from './Hover';
+// import withHover from './withHover';
 
 const styles = {
   container: {
@@ -67,20 +68,34 @@ const styles = {
 //   }
 // }
 
-// to reuse hover logic we can use HOC
-function Tooltip({ text, children, isHover }) {
+// HOC approch, to reuse hover logic we can use HOC
+// function Tooltip({ text, children, isHover }) {
+//   return (
+//     <div style={styles.container}>
+//       {isHover === true && <div style={styles.tooltip}>{text}</div>}
+//       {children}
+//     </div>
+//   );
+// }
+
+// Render prop approach, to solve the naming collision and be more explixit about how component is render
+export default function Tooltip({ text, children }) {
   return (
-    <div style={styles.container}>
-      {isHover === true && <div style={styles.tooltip}>{text}</div>}
-      {children}
-    </div>
+    <Hover>
+      {isHover => (
+        <div style={styles.container}>
+          {isHover === true && <div style={styles.tooltip}>{text}</div>}
+          {children}
+        </div>
+      )}
+    </Hover>
   );
 }
 
 Tooltip.propTypes = {
   text: PropTypes.string.isRequired,
   children: PropTypes.element.isRequired,
-  isHover: PropTypes.bool.isRequired,
+  // isHover: PropTypes.bool.isRequired, only need in HOC, because now we are not passing it to the component
 };
 
-export default withHover(Tooltip, 'isHover');
+// export default withHover(Tooltip, 'isHover'); HOC method
