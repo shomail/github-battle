e185550b186ff6a61ac13e19d1a84d28bd487eb1
shomail <shomail1988@gmail.com>
2019-08-02 23:18:12 +0200

    Nav component and Theme toggle button

--- app/index.js
+++ app/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import Popluar from './components/Popular';
 import Battle from './components/Battle';
 import { ThemeProvider } from './contexts/theme';
+import Nav from './components/Nav';
 
 class App extends React.Component {
   constructor(props) {
@@ -22,9 +23,12 @@ class App extends React.Component {
   render() {
     return (
       <ThemeProvider value={this.state}>
-        <div className="container">
-          <h1>Github Explorer</h1>
-          <Battle />
+        <div className={this.state.theme}>
+          <div className="container">
+            <Nav />
+            <h1>Github Explorer</h1>
+            <Battle />
+          </div>
         </div>
       </ThemeProvider>
     );
