e9551fbf596725986401e1496c56a48f3d5ed955
shomail <shomail1988@gmail.com>
2019-08-03 21:57:46 +0200

    themefied PlayerPreview component

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -107,24 +107,32 @@ PlayerInput.propTypes = {
 };
 
 const PlayerPreview = ({ username, onReset, label }) => (
-  <div className="column player">
-    <h3 className="player-label">{label}</h3>
-    <div className="row bg-light">
-      <div className="player-info">
-        <img
-          className="avatar-small"
-          src={`http://github.com/${username}.png?size=200`}
-          alt={`Avatar for ${username}`}
-        />
-        <a href={`http://github.com/${username}`} className="link">
-          {username}
-        </a>
+  <ThemeConsumer>
+    {({ theme }) => (
+      <div className="column player">
+        <h3 className="player-label">{label}</h3>
+        <div className={`row bg-${theme}`}>
+          <div className="player-info">
+            <img
+              className="avatar-small"
+              src={`http://github.com/${username}.png?size=200`}
+              alt={`Avatar for ${username}`}
+            />
+            <a href={`http://github.com/${username}`} className="link">
+              {username}
+            </a>
+          </div>
+          <button
+            type="button"
+            className="btn-clear flex-center"
+            onClick={onReset}
+          >
+            <FaTimesCircle color="rgb(194, 57, 42)" size={26} />
+          </button>
+        </div>
       </div>
-      <button className="btn-clear flex-center" onClick={onReset}>
-        <FaTimesCircle color="rgb(194, 57, 42)" size={26} />
-      </button>
-    </div>
-  </div>
+    )}
+  </ThemeConsumer>
 );
 
 PlayerPreview.propTypes = {
