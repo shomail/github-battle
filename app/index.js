6082ffcf251596838f27dc633b27a003126a549d
shomail <shomail1988@gmail.com>
2019-08-04 20:49:56 +0200

    class properties in index component

--- app/index.js
+++ app/index.js
@@ -9,18 +9,14 @@ import { ThemeProvider } from './contexts/theme';
 import Nav from './components/Nav';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      theme: 'light',
-      toggleTheme: () => {
-        this.setState(({ theme }) => ({
-          theme: theme === 'light' ? 'dark' : 'light',
-        }));
-      },
-    };
-  }
+  state = {
+    theme: 'light',
+    toggleTheme: () => {
+      this.setState(({ theme }) => ({
+        theme: theme === 'light' ? 'dark' : 'light',
+      }));
+    },
+  };
 
   render() {
     const { theme, toggleTheme } = this.state;
